5606b9dec7c0dc3e1cefee67c26d5afaad66f2b0
hayadebi <76168948+hayadebi@users.noreply.github.com>
2026-03-06 14:13:31 +0900

    a
    a

--- Ads/AdstirSwitch.js
+++ Ads/AdstirSwitch.js
@@ -1,22 +1,31 @@
-  var PC_AD_HTML = '<div style="width:100%;max-width:100%;height:auto;overflow-wrap:break-word;"><center><script type="text/javascript">var adstir_vars={ver:"4.0",app_id:"MEDIA-8d0588f8",ad_spot:1,center:false};<\/script><script type="text/javascript" src="https://js.ad-stir.com/js/adstir.js"><\/script><\/center><\/div>';
+  (function() {
+    var BREAKPOINT = 768;
 
-  var SP_AD_HTML = '<div style="width:100%;max-width:100%;height:auto;overflow-wrap:break-word;"><script type="text/javascript">var adstir_vars={ver:"4.0",type:"native",app_id:"MEDIA-8d0588f8",ad_spot:3};<\/script><script type="text/javascript" src="https://js.ad-stir.com/js/adstir_native.js"><\/script><\/div>';
+    function showAd() {
+      var wrap = document.querySelector('.adstir-responsive-wrap');
+      if (!wrap) return;
 
-  function isPC() {
-    return !/Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent) || window.innerWidth >= 768;
-  }
+      var pcAd = wrap.querySelector('.adstir-pc');
+      var spAd = wrap.querySelector('.adstir-sp');
 
-  function injectAds() {
-    var adCode = isPC() ? PC_AD_HTML : SP_AD_HTML;
-    var areas = document.querySelectorAll('div.adstir-area');
-    for (var i = 0; i < areas.length; i++) {
-      var range = document.createRange();
-      areas[i].appendChild(range.createContextualFragment(adCode));
+      if (window.innerWidth >= BREAKPOINT) {
+        if (pcAd) pcAd.style.display = 'block';
+        if (spAd) spAd.style.display = 'none';
+      } else {
+        if (pcAd) pcAd.style.display = 'none';
+        if (spAd) spAd.style.display = 'block';
+      }
     }
-  }
 
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', injectAds);
-  } else {
-    injectAds();
-  }
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', showAd);
+    } else {
+      showAd();
+    }
+
+    var resizeTimer;
+    window.addEventListener('resize', function() {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(showAd, 200);
+    });
+  })();
